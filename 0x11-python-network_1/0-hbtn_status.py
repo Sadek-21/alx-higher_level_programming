ca144d2b806e8e9422d6f145e1f77a14b78f1970
SADEK <mohamed20000327@gmail.com>
2024-07-06 14:23:38 +0100

    updt

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        r = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(r)))
-        print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+header = {'User-Agent': 'Mozilla/5.0'}
+req = urllib.request.Request(
+        'https://intranet.hbtn.io/status', headers=header
+)
+
+with urllib.request.urlopen(req) as response:
+    res = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(res.__class__))
+    print("\t- content: {}".format(res))
+    print("\t- utf8 content: {}".format(res.decode('ascii')))
